d383ce694259354b872c99784652aae2ebbb86fc
Pedro Bortolli <pedro.vitor.santos@usp.br>
2019-07-01 02:18:20 +0000

    Add progress bar component

---
+++ EP3/client/src/components/Progress.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Line} from 'rc-progress'
+import styled from 'styled-components'
+
+const ProgressContainer = styled.div`
+    display: grid;
+    grid-template-columns: 130px 200px 50px; 
+    align-items: center;
+`
+const Title = styled.span`
+    text-align: end;
+    padding-right: 20px;
+    font-size: 22px;
+`
+const Numbers = styled.span`
+    font-size: 22px;
+    padding-left: 20px;
+`
+
+function Progress({title, done, total, color, stroke = 5}) {
+    const percent = (done/total)*100
+    return (
+        <ProgressContainer>
+            <Title>{title}:</Title>
+            <Line percent={percent} strokeWidth={stroke} strokeColor={color} />
+            <Numbers>{done}/{total}</Numbers>
+        </ProgressContainer>
+    )
+}
+
+export default Progress
--- EP3/client/src/pages/Dashboard.js
+++ EP3/client/src/pages/Dashboard.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import styled from 'styled-components'
 import Card from '../components/Card'
+import Progress from '../components/Progress'
 
 const Container = styled.div`
     padding-top: 50px;
@@ -31,6 +32,10 @@ function Dashboard() {
     const temp = [{code: 'MAC0329', title:	'Álgebra Booleana e Aplicações no Projeto de Arquitetura de Computadores'}, {code: 'MAC0450', title:	'Algoritmos de Aproximação'}, {code: 'MAC0430', title:	'Algoritmos e Complexidade de Computação'}, {code: 'MAC0121', title:	'Algoritmos e Estruturas de Dados I'}, {code: 'MAC0323', title:	'Algoritmos e Estruturas de Dados II'}, {code: 'MAC0351', title:	'Algoritmos em Bioinformática'}, {code: 'MAC0328', title:	'Algoritmos em Grafos'}, {code: 'MAC0338', title:	'Análise de Algoritmos'}, {code: 'MAC0447', title:	'Análise e Reconhecimento de Formas: Teoria e Prática'}, {code: 'MAC0333', title:	'Armazenamento e Recuperação de Informação'}, {code: 'MAC0344', title:	'Arquitetura de Computadores'}, {code: 'MAC0213', title:	'Atividade Curricular em Comunidade'}, {code: 'MAC0214', title:	'Atividade Curricular em Cultura e Extensão'}, {code: 'MAC0215', title:	'Atividade Curricular em Pesquisa'}, {code: 'MAC0414', title:	'Autômatos, Computabilidade e Complexidade'}, {code: 'MAC0465', title:	'Biologia Computacional'}, {code: 'MAC0375', title:	'Biologia de Sistemas'}, {code: 'MAC0102', title:	'Caminhos no Bacharelado em Ciência da Computação'}, {code: 'MAC0459', title:	'Ciência e Engenharia de Dados'}, {code: 'MAC0463', title:	'Computação Móvel'}, {code: 'MAC0337', title:	'Computação Musical'}, {code: 'MAC0326', title:	'Computação, Cibernética e Sistemas Cognitivos.'}, {code: 'MAC0316', title:	'Conceitos Fundamentais de Linguagens de Programação'}, {code: 'MAC0469', title:	'Construção de Software como Serviço em Computação em Nuvem'}, {code: 'MAC0336', title:	'Criptografia para Segurança de Dados'}]
     return (
         <Container>
+            <Progress title="Obrigatórias" done={111} total={111} color="green" />
+            <Progress title="Eletivas" done={40} total={52} color="blue" />
+            <Progress title="Livres" done={6} total={24} color="orange" />
+
             <h1>Matérias selecionadas</h1>
             <SelectedCourses>
                 {selected.map(code => <span>{code}</span>)}
