ab08abce303300fcf678af40035cf767c673291f
Pedro Bortolli <pedro.vitor.santos@usp.br>
2019-07-04 21:42:53 +0000

    Add padding to not found page

--- EP3/client/src/pages/NotFound.js
+++ EP3/client/src/pages/NotFound.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function NotFound() {
-    return <h1>404: Página não encontrada</h1>
+    return <h1 style={{padding: '60 0 0 20'}}>404: Página não encontrada</h1>
 }
 
 export default NotFound
