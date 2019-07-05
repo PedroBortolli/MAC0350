8ad352c7717eff0c4552976b2e1ac7724f65e647
Pedro Sousa <pedroteosousa@gmail.com>
2019-07-04 21:39:45 -0300

    Add more info to each course on mock

--- EP3/server/api/mocks.js
+++ EP3/server/api/mocks.js
@@ -1,48 +1,36 @@
 module.exports = {
   disciplinas: [
     {
-      code: "MAC0329",
-      title:
-        "Álgebra Booleana e Aplicações no Projeto de Arquitetura de Computadores"
-    },
-    { code: "MAC0450", title: "Algoritmos de Aproximação" },
-    { code: "MAC0430", title: "Algoritmos e Complexidade de Computação" },
-    { code: "MAC0121", title: "Algoritmos e Estruturas de Dados I" },
-    { code: "MAC0323", title: "Algoritmos e Estruturas de Dados II" },
-    { code: "MAC0351", title: "Algoritmos em Bioinformática" },
-    { code: "MAC0328", title: "Algoritmos em Grafos" },
-    { code: "MAC0338", title: "Análise de Algoritmos" },
-    {
-      code: "MAC0447",
-      title: "Análise e Reconhecimento de Formas: Teoria e Prática"
-    },
-    { code: "MAC0333", title: "Armazenamento e Recuperação de Informação" },
-    { code: "MAC0344", title: "Arquitetura de Computadores" },
-    { code: "MAC0213", title: "Atividade Curricular em Comunidade" },
-    { code: "MAC0214", title: "Atividade Curricular em Cultura e Extensão" },
-    { code: "MAC0215", title: "Atividade Curricular em Pesquisa" },
-    { code: "MAC0414", title: "Autômatos, Computabilidade e Complexidade" },
-    { code: "MAC0465", title: "Biologia Computacional" },
-    { code: "MAC0375", title: "Biologia de Sistemas" },
-    {
-      code: "MAC0102",
-      title: "Caminhos no Bacharelado em Ciência da Computação"
-    },
-    { code: "MAC0459", title: "Ciência e Engenharia de Dados" },
-    { code: "MAC0463", title: "Computação Móvel" },
-    { code: "MAC0337", title: "Computação Musical" },
-    {
-      code: "MAC0326",
-      title: "Computação, Cibernética e Sistemas Cognitivos."
+      codigo: "MAC0329",
+      titulo: "Álgebra Booleana e Aplicações no Projeto de Arquitetura de Computadores",
+      creditos: {
+        aula: 4,
+        trabalho: 2,
+      }
     },
     {
-      code: "MAC0316",
-      title: "Conceitos Fundamentais de Linguagens de Programação"
+      code: "MAC0450",
+      title: "Algoritmos de Aproximação",
+      creditos: {
+        aula: 4,
+        trabalho: 2,
+      }
     },
     {
-      code: "MAC0469",
-      title: "Construção de Software como Serviço em Computação em Nuvem"
+      code: "MAC0430",
+      title: "Algoritmos e Complexidade de Computação",
+      creditos: {
+        aula: 4,
+        trabalho: 2,
+      }
     },
-    { code: "MAC0336", title: "Criptografia para Segurança de Dados" }
+    {
+      code: "MAC0121",
+      title: "Algoritmos e Estruturas de Dados I",
+      creditos: {
+        aula: 4,
+        trabalho: 2,
+      }
+    },
   ]
 }
