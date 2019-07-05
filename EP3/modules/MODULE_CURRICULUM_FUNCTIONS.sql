985b72d9b61a6619495c6886b44cc50973399587
Pedro Bortolli <pedro.vitor.santos@usp.br>
2019-07-05 20:36:57 +0000

    Add functions for curriculum module

---
+++ EP3/modules/MODULE_CURRICULUM_FUNCTIONS.sql
@@ -0,0 +1,36 @@
+DROP FUNCTION IF EXISTS cria_curriculo(INTEGER, VARCHAR(50), INTEGER);
+DROP FUNCTION IF EXISTS cria_trilha(VARCHAR(50), VARCHAR(255), INTEGER);
+DROP FUNCTION IF EXISTS cria_modulo(INTEGER, VARCHAR(50), VARCHAR(255), INTEGER);
+DROP FUNCTION IF EXISTS cria_disciplina(VARCHAR(7), VARCHAR(100), INTEGER, INTEGER, VARCHAR(50));
+
+CREATE OR REPLACE FUNCTION cria_curriculo(INTEGER, VARCHAR(50), INTEGER) RETURNS VOID
+LANGUAGE plpgsql
+AS $$
+	BEGIN
+		INSERT INTO curriculo(codigo, nome, id_adm) VALUES ($1, $2, $3);
+	END
+$$;
+
+CREATE OR REPLACE FUNCTION cria_trilha(VARCHAR(50), VARCHAR(255), INTEGER) RETURNS VOID
+LANGUAGE plpgsql
+AS $$
+	BEGIN
+		INSERT INTO trilha(nome, descricao, quant_mod) VALUES ($1, $2, $3);
+	END
+$$;
+
+CREATE OR REPLACE FUNCTION cria_modulo(INTEGER, VARCHAR(50), VARCHAR(255), INTEGER) RETURNS VOID 
+LANGUAGE plpgsql
+AS $$
+	BEGIN
+		INSERT INTO modulo(id_trilha, nome, descricao, quant_disc) VALUES ($1, $2, $3);
+	END
+$$;
+
+CREATE OR REPLACE FUNCTION cria_disciplina(VARCHAR(7), VARCHAR(100), INTEGER, INTEGER, VARCHAR(50)) RETURNS VOID
+LANGUAGE plpgsql
+AS $$
+	BEGIN
+		INSERT INTO disciplina(codigo, nome, creditos_aula, creditos_trabalho, instituto) VALUES ($1, $2, $3, $4, $5);
+	END
+$$;
