1cb61b13e3238359d85fbaef8b04bc3af9a51576
Pedro Sousa <pedroteosousa@gmail.com>
2019-07-05 20:28:41 -0300

    Use local db

--- EP3/server/database.js
+++ EP3/server/database.js
@@ -1,8 +1,11 @@
 const Pool = require('pg').Pool
 
 const client = new Pool({
-    connectionString: 'postgres://ujfnmbijvmtsja:287a9ec92486cf03e55140f88792d1d4062777bbe17baace3377d3f488495b3a@ec2-23-21-160-38.compute-1.amazonaws.com:5432/d28drvnsoe5apn',
-    ssl: true,
+    user: 'postgres',
+    password: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    database: 'postgres'
 })
 client.connect()
 
