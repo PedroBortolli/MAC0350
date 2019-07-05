a1fd4c7d951070564029ce44c7f1ef591060b5dc
Pedro Sousa <pedroteosousa@gmail.com>
2019-07-04 21:31:36 -0300

    Get courses from api

--- EP3/client/src/pages/Dashboard.js
+++ EP3/client/src/pages/Dashboard.js
@@ -1,9 +1,10 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import Card from '../components/Card'
 import Progress from '../components/Progress'
 import useScreenSize from '../hooks/useScreenSize'
 import Modal from '../components/Modal'
+import fetchApi from '../utils/fetchApi'
 
 const Container = styled.div`
     padding-top: 60px;
@@ -77,6 +78,7 @@ const CurrentCourses = styled.div`
 `
 
 function Dashboard() {
+    const [courses, setCourses] = useState([])
     const [currentCourses, changeCurrentCourses] = useState([])
     const [scheduledCourses, changeScheduledCourses] = useState([])
     const [doneCourses, changeDoneCourses] = useState([])
@@ -85,8 +87,14 @@ function Dashboard() {
     const [width] = useScreenSize()
     const columns = Math.min(Math.floor((width-330)/270), 3)
 
-    const temp = [{code: 'MAC0329', title:	'Álgebra Booleana e Aplicações no Projeto de Arquitetura de Computadores'}, {code: 'MAC0450', title:	'Algoritmos de Aproximação'}, {code: 'MAC0430', title:	'Algoritmos e Complexidade de Computação'}, {code: 'MAC0121', title:	'Algoritmos e Estruturas de Dados I'}, {code: 'MAC0323', title:	'Algoritmos e Estruturas de Dados II'}, {code: 'MAC0351', title:	'Algoritmos em Bioinformática'}, {code: 'MAC0328', title:	'Algoritmos em Grafos'}, {code: 'MAC0338', title:	'Análise de Algoritmos'}, {code: 'MAC0447', title:	'Análise e Reconhecimento de Formas: Teoria e Prática'}, {code: 'MAC0333', title:	'Armazenamento e Recuperação de Informação'}, {code: 'MAC0344', title:	'Arquitetura de Computadores'}, {code: 'MAC0213', title:	'Atividade Curricular em Comunidade'}, {code: 'MAC0214', title:	'Atividade Curricular em Cultura e Extensão'}, {code: 'MAC0215', title:	'Atividade Curricular em Pesquisa'}, {code: 'MAC0414', title:	'Autômatos, Computabilidade e Complexidade'}, {code: 'MAC0465', title:	'Biologia Computacional'}, {code: 'MAC0375', title:	'Biologia de Sistemas'}, {code: 'MAC0102', title:	'Caminhos no Bacharelado em Ciência da Computação'}, {code: 'MAC0459', title:	'Ciência e Engenharia de Dados'}, {code: 'MAC0463', title:	'Computação Móvel'}, {code: 'MAC0337', title:	'Computação Musical'}, {code: 'MAC0326', title:	'Computação, Cibernética e Sistemas Cognitivos.'}, {code: 'MAC0316', title:	'Conceitos Fundamentais de Linguagens de Programação'}, {code: 'MAC0469', title:	'Construção de Software como Serviço em Computação em Nuvem'}, {code: 'MAC0336', title:	'Criptografia para Segurança de Dados'}]
-    
+    useEffect(() => {
+        const fetchDisciplinas = async () => {
+            setCourses((await fetchApi('GET', 'http://localhost:5000/api/disciplinas')).data)
+        }
+
+        fetchDisciplinas()
+    }, [])
+
     const display = (course) => {
         if (currentCourses.some(c => c.code === course.code) ||
             doneCourses.some(c => c.code === course.code) ||
@@ -134,7 +142,7 @@ function Dashboard() {
                     <h1>Matérias disponíveis</h1>
                     <input placeholder="Procure por uma disciplina..." onChange={e => changeFilter(e.target.value)} />
                     <CardsContainer columns={columns}>
-                        {temp.map(course => {
+                        {courses.map(course => {
                             if (!display(course)) return null
                             return <Card key={course.code} course={course} status="available" set={changeModal}  />
                         })}
