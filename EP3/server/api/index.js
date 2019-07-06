275413f750b886093fe4f3ea4f6f02007d6d80cb
Pedro Sousa <pedroteosousa@gmail.com>
2019-07-06 18:35:08 -0300

    Add more routes for access module

--- EP3/server/api/index.js
+++ EP3/server/api/index.js
@@ -3,9 +3,13 @@ const router = require('express').Router()
 const authRoutes = require('./auth')
 const disciplinasRoutes = require('./disciplinas')
 const servicosRoutes = require('./servicos')
+const usuariosRoutes = require('./usuarios')
+const perfisRoutes = require('./perfis')
 
 router.use('/auth', authRoutes)
 router.use('/disciplinas', disciplinasRoutes)
 router.use('/servicos', servicosRoutes)
+router.use('/usuarios', usuariosRoutes)
+router.use('/perfis', perfisRoutes)
 
 module.exports = router
--- EP3/server/api/perfis.js
+++ EP3/server/api/perfis.js
@@ -5,7 +5,7 @@ const client = require('../database')
 // lembrar de ver se o cara pode fazer isso
 router.get('/', (req, res) => {
     client.query({
-        text: 'SELECT * FROM servico;',
+        text: 'SELECT * FROM perfil;',
     }).then(({ rows }) => {
         res.send(rows || []).status(200)
     }).catch(err => {
@@ -16,16 +16,16 @@ router.get('/', (req, res) => {
 
 // lembrar de ver se o cara pode fazer isso
 router.post('/create', (req, res) => {
-    if (!req.body.servico) {
+    if (!req.body.perfil) {
         return res.sendStatus(400)
     }
-    const { tipo, descricao } = req.body.servico;
-    if (!tipo || !descricao) {
-        return res.sendStatus(400)
+    const { papel, descricao } = req.body.servico;
+    if (!papel || !descricao) {
+        res.sendStatus(400)
     }
     client.query({
-        text: 'SELECT cria_servico ($1, $2);',
-        values: [tipo, descricao],
+        text: 'SELECT cria_perfil ($1, $2);',
+        values: [papel, descricao],
     }).then(() => {
         res.sendStatus(200)
     }).catch(err => {
@@ -36,16 +36,16 @@ router.post('/create', (req, res) => {
 
 // lembrar de ver se o cara pode fazer isso
 router.delete('/', (req, res) => {
-    if (!req.body.servico) {
-        return res.sendStatus(400)
+    if (!req.body.perfil) {
+        res.sendStatus(400)
     }
-    const { tipo } = req.body.servico
-    if (!tipo) {
+    const { papel } = req.body.perfil
+    if (!papel) {
         return res.sendStatus(400)
     }
     client.query({
-        text: 'SELECT remove_servico ($1);',
-        values: [tipo],
+        text: 'SELECT remove_perfil ($1);',
+        values: [papel],
     }).then(() => {
         res.sendStatus(200)
     }).catch(err => {
