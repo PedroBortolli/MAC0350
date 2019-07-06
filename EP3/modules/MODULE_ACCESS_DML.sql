2d8d4699f287ec77a2ad664bfac89f18a5dcaff0
Pedro Sousa <pedroteosousa@gmail.com>
2019-07-06 17:42:10 -0300

    Fill access module with functions

--- EP3/modules/MODULE_ACCESS_DML.sql
+++ EP3/modules/MODULE_ACCESS_DML.sql
@@ -1,9 +1,28 @@
 SELECT cria_usuario ('admin', 'admin@admin.com', 'password');
 SELECT cria_usuario ('cef', 'cef@ime.usp.br', 'euamoacris');
-SELECT cria_usuario ('pedroteosousa', 'pedro.teotonio.sousa@usp.br', 'senhasenhasenha')
+SELECT cria_usuario ('pedroteosousa', 'pedro.teotonio.sousa@usp.br', 'senhasenhasenha');
 
-SELECT cria_perfil ('aluno', 'tem acesso a comandos de selecionar e mudar coisas relacionadas ao seu usuário');
+SELECT cria_perfil ('aluno', 'tem acesso a comandos de selecionar');
+SELECT cria_perfil ('administrador', 'tem acesso a comandos de selecionar, criar e remover');
 
-SELECT cria_servico ('remoção', 'permite remover campos no db');
-SELECT cria_servico ('criação', 'permite criar campos no db');
+SELECT cria_servico ('visualização', 'permite visualizar campos no db');
 SELECT cria_servico ('atualização', 'permite atualizar campos no db');
+SELECT cria_servico ('criação', 'permite criar campos no db');
+SELECT cria_servico ('remoção', 'permite remover campos no db');
+
+SELECT associa_perfil ('pedroteosousa', 'aluno');
+SELECT associa_perfil ('cef', 'administrador');
+
+SELECT associa_servicos ('aluno', 'visualização');
+SELECT associa_servicos ('administrador', 'visualização');
+SELECT associa_servicos ('administrador', 'atualização');
+SELECT associa_servicos ('administrador', 'criação');
+SELECT associa_servicos ('administrador', 'remoção');
+
+-- SELECT * FROM seleciona_perfis ('pedroteosousa');
+-- SELECT * FROM seleciona_perfis ('cef');
+
+-- SELECT * FROM seleciona_servicos ('aluno');
+-- SELECT * FROM seleciona_servicos ('administrador');
+
+-- SELECT * FROM seleciona_servicos_usuario ('cef');
--- EP3/modules/MODULE_ACCESS_FUNCTIONS.sql
+++ EP3/modules/MODULE_ACCESS_FUNCTIONS.sql
@@ -1,12 +1,25 @@
-DROP FUNCTION IF EXISTS cria_usuario(VARCHAR(50), VARCHAR(100), VARCHAR(50));
-DROP FUNCTION IF EXISTS seleciona_usuario(VARCHAR(50));
-DROP FUNCTION IF EXISTS atualiza_usuario_email(VARCHAR(50), VARCHAR(100));
-DROP FUNCTION IF EXISTS remove_usuario(VARCHAR(50));
+DROP FUNCTION IF EXISTS cria_usuario (VARCHAR(50), VARCHAR(100), VARCHAR(50));
+DROP FUNCTION IF EXISTS seleciona_usuario (VARCHAR(50));
+DROP FUNCTION IF EXISTS atualiza_usuario_email (VARCHAR(50), VARCHAR(100));
+DROP FUNCTION IF EXISTS remove_usuario (VARCHAR(50));
 
-DROP FUNCTION IF EXISTS cria_perfil (VARCHAR(50));
+DROP FUNCTION IF EXISTS cria_perfil (VARCHAR(50), TEXT);
 DROP FUNCTION IF EXISTS seleciona_perfil (VARCHAR(50));
+DROP FUNCTION IF EXISTS atualiza_perfil_descricao (VARCHAR(50), TEXT);
+DROP FUNCTION IF EXISTS remove_perfil (VARCHAR(50));
 
-DROP FUNCTION IF EXISTS cria_servico (VARCHAR(50));
+DROP FUNCTION IF EXISTS cria_servico (VARCHAR(50), TEXT);
+DROP FUNCTION IF EXISTS seleciona_servico (VARCHAR(50));
+DROP FUNCTION IF EXISTS atualiza_servico_descricao (VARCHAR(50), TEXT);
+DROP FUNCTION IF EXISTS remove_servico (VARCHAR(50));
+
+DROP FUNCTION IF EXISTS associa_perfil (VARCHAR(50), VARCHAR(50));
+DROP FUNCTION IF EXISTS seleciona_perfis (VARCHAR(50));
+
+DROP FUNCTION IF EXISTS associa_servicos (VARCHAR(50), VARCHAR(50));
+DROP FUNCTION IF EXISTS seleciona_servicos (VARCHAR(50));
+
+-- USUARIO
 
 CREATE OR REPLACE FUNCTION cria_usuario (VARCHAR(50), VARCHAR(100), VARCHAR(50)) RETURNS VOID
 LANGUAGE plpgsql
@@ -47,7 +60,9 @@ BEGIN
 END;
 $$;
 
-CREATE OR REPLACE FUNCTION cria_perfil (VARCHAR(50), text) RETURNS VOID
+-- PERFIL
+
+CREATE OR REPLACE FUNCTION cria_perfil (VARCHAR(50), TEXT) RETURNS VOID
 LANGUAGE plpgsql
 AS $$
 BEGIN
@@ -58,7 +73,7 @@ $$;
 CREATE OR REPLACE FUNCTION seleciona_perfil (VARCHAR(50))
 RETURNS TABLE(
     papel VARCHAR(50),
-    descricao VARCHAR(100),
+    descricao TEXT
 )
 LANGUAGE plpgsql
 AS $$
@@ -67,10 +82,127 @@ BEGIN
 END;
 $$;
 
-CREATE OR REPLACE FUNCTION cria_servico (VARCHAR(50), text) RETURNS VOID
+CREATE OR REPLACE FUNCTION atualiza_perfil_descricao (VARCHAR(50), TEXT) RETURNS VOID
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    UPDATE perfil
+    SET descricao = $2
+    WHERE papel = $1;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION remove_perfil (VARCHAR(50)) RETURNS VOID
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    DELETE FROM perfil WHERE papel = $1;
+END;
+$$;
+
+-- SERVICO
+
+CREATE OR REPLACE FUNCTION cria_servico (VARCHAR(50), TEXT) RETURNS VOID
 LANGUAGE plpgsql
 AS $$
 BEGIN
     INSERT INTO servico (tipo, descricao) VALUES ($1, $2);
+END;
+$$;
+
+
+
+CREATE OR REPLACE FUNCTION seleciona_servico (VARCHAR(50))
+RETURNS TABLE(
+    tipo VARCHAR(50),
+    descricao TEXT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN QUERY SELECT * FROM servico WHERE servico.tipo = $1;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION atualiza_servico_descricao (VARCHAR(50), TEXT) RETURNS VOID
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    UPDATE servico
+    SET descricao = $2
+    WHERE tipo = $1;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION remove_servico (VARCHAR(50)) RETURNS VOID
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    DELETE FROM servico WHERE tipo = $1;
+END;
+$$;
+
+-- REL_US_PF
+
+CREATE OR REPLACE FUNCTION associa_perfil (VARCHAR(50), VARCHAR(50)) RETURNS VOID
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO rel_us_pf (usuario_id, perfil_id) VALUES ($1, $2);
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION seleciona_perfis (VARCHAR(50))
+RETURNS TABLE (
+    papel VARCHAR(50),
+    descricao TEXT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN QUERY SELECT perfil.papel, perfil.descricao FROM perfil
+    INNER JOIN rel_us_pf ON perfil.papel = rel_us_pf.perfil_id
+    WHERE rel_us_pf.usuario_id = $1;
+END;
+$$;
+
+-- REL_PF_SE
+
+CREATE OR REPLACE FUNCTION associa_servicos (VARCHAR(50), VARCHAR(50)) RETURNS VOID
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO rel_pf_se (perfil_id, servico_id) VALUES ($1, $2);
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION seleciona_servicos (VARCHAR(50))
+RETURNS TABLE (
+    tipo VARCHAR(50),
+    descricao TEXT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN QUERY SELECT servico.tipo, servico.descricao FROM servico
+    INNER JOIN rel_pf_se ON servico.tipo = rel_pf_se.servico_id
+    WHERE rel_pf_se.perfil_id = $1;
+END;
+$$;
+
+-- N EXISTE, MAS REL_US_SE
+
+CREATE OR REPLACE FUNCTION seleciona_servicos_usuario (VARCHAR(50))
+RETURNS TABLE (
+    tipo VARCHAR(50),
+    descricao TEXT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN QUERY SELECT servico.tipo, servico.descricao FROM servico
+    INNER JOIN rel_pf_se ON servico.tipo = rel_pf_se.servico_id
+    INNER JOIN rel_us_pf ON rel_pf_se.perfil_id = rel_us_pf.perfil_id
+    WHERE rel_us_pf.usuario_id = $1;
 END;
 $$;
