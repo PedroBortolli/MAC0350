cfdffc25e731665a739e2c3dce2025fdd3e0cde0
Pedro Sousa <pedroteosousa@gmail.com>
2019-07-06 14:19:52 -0300

    Add route to create a 'serviço'

--- EP3/server/api/servicos.js
+++ EP3/server/api/servicos.js
@@ -13,4 +13,24 @@ router.get('/', (req, res) => {
     })
 })
 
+router.post('/create', (req, res) => {
+    try {
+        const { tipo, descricao } = req.body.servico;
+        if (!tipo || !descricao) {
+            res.sendStatus(400)
+        }
+        client.query({
+            text: 'INSERT INTO servico (tipo, descricao) VALUES ($1, $2);',
+            values: [tipo, descricao],
+        }).then(() => {
+            res.sendStatus(200)
+        }).catch(err => {
+            console.error(err)
+            res.sendStatus(500)
+        })
+    } catch (err) {
+        res.sendStatus(400)
+    }
+})
+
 module.exports = router
