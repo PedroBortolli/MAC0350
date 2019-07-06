6a8b38912c0ceb2ed15939cd08d01445df9edb77
Pedro Sousa <pedroteosousa@gmail.com>
2019-07-06 13:48:29 -0300

    Move mocks

--- EP3/server/api/disciplinas.js
+++ EP3/server/api/disciplinas.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
-const disciplinas = require('./mocks').disciplinas
+const disciplinas = require('./common/mocks').disciplinas
 
 router.get('/', (req, res) => {
     res.send({ data: disciplinas }).status(200)
