20d3b017a5d268e1167abc3e20bc68a866a51823
Pedro Sousa <pedroteosousa@gmail.com>
2019-07-07 18:34:13 -0300

    Add an authorization middleware

---
+++ EP3/server/api/common/authorize.js
@@ -0,0 +1,37 @@
+const client = require('../../database')
+
+module.exports = (tipo) => (req, res, next) => {
+    if (!req.body.auth) {
+        return res.sendStatus(401)
+    }
+    const { login, senha } = req.body.auth;
+    if (login === undefined || senha === undefined) {
+        return res.sendStatus(401)
+    }
+    client.mod_acc.query({
+        text: 'SELECT * FROM seleciona_usuario ($1)',
+        values: [login],
+    }).then((result) => {
+        const user = result.rows[0]
+        if (user.senha !== senha) {
+            return res.sendStatus(401)
+        }
+        client.mod_acc.query({
+            text: 'SELECT * FROM seleciona_servicos_usuario ($1);',
+            values: [login],
+        }).then(({ rows }) => {
+            const permissions = rows.map(row => row.tipo)
+            if (permissions.includes(tipo)) {
+                next()
+            } else {
+                return res.sendStatus(403)
+            }
+        }).catch(err => {
+            console.error(err)
+            res.sendStatus(500)
+        })
+    }).catch(err => {
+        console.error(err)
+        res.sendStatus(500)
+    })
+}
