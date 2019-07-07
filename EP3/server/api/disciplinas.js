bef196067836e1e44fbf2dc8cc46944ce7d23f23
Pedro Sousa <pedroteosousa@gmail.com>
2019-07-07 02:14:56 -0300

    Get 'disciplinas' from db

--- EP3/server/api/disciplinas.js
+++ EP3/server/api/disciplinas.js
@@ -1,9 +1,16 @@
 const router = require('express').Router()
 
-const disciplinas = require('./common/mocks').disciplinas
+const client = require('../database')
 
 router.get('/', (req, res) => {
-    res.send({ data: disciplinas }).status(200)
+    client.mod_cur.query({
+        text: 'SELECT * FROM disciplina;',
+    }).then(({ rows }) => {
+        res.send(rows || []).status(200)
+    }).catch(err => {
+        console.error(err)
+        res.sendStatus(500)
+    })
 })
 
 module.exports = router
