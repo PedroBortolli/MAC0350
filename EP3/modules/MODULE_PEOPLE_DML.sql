ea8ddae6f70d91c3c8169941007e72e2cf1a6d31
Rubens Douglas <rubensdouglas@gmail.com>
2019-07-07 05:23:08 +0000

    Fix MODULE_PEOPLE_DML

--- EP3/modules/MODULE_PEOPLE_DML.sql
+++ EP3/modules/MODULE_PEOPLE_DML.sql
@@ -22,39 +22,35 @@ SELECT cria_pessoa('Roberto');
 SELECT cria_pessoa('Nina');
 SELECT cria_pessoa('Decio');
 
-
 SELECT cria_aluno('Bortolli', 2016);
-INSERT INTO aluno(id_aluno, ano_ingresso) VALUES
-	(2, 2016),
-	(3, 2015),
-	(4, 2016),
-	(5, 2015),
-	(6, 2017),
-	(7, 2016),
-	(8, 2017),
-	(9, 2015),
-	(10, 2017);
+SELECT cria_aluno('Teotonio', 2016);
+SELECT cria_aluno('Daher', 2015);
+SELECT cria_aluno('Jonas', 2016);
+SELECT cria_aluno('Enzo', 2015);
+SELECT cria_aluno('Moretto', 2017);
+SELECT cria_aluno('Rubens', 2016);
+SELECT cria_aluno('Thiago', 2017);
+SELECT cria_aluno('Ricardo', 2015);
+SELECT cria_aluno('Breno', 2017);
 
 SELECT cria_professor('Jef', 'Banco de dados');
-INSERT INTO professor(id_professor, formacao_area) VALUES
-	(12, 'Teoria'),
-	(13, 'Machine Learning'),
-	(14, 'IA'),
-	(15, 'Sistemas'),
-	(16, 'Sistemas'),
-	(17, 'Teoria'),
-	(18, 'Teoria'),
-	(19, 'Machine Learning'),
-	(20, 'Machine Learning');
+SELECT cria_professor('Carlinhos', 'Teoria');
+SELECT cria_professor('Hirata', 'Machie Learning');
+SELECT cria_professor('Renata', 'IA');
+SELECT cria_professor('Daniel', 'Sistemas');
+SELECT cria_professor('Fabio', 'Sistemas');
+SELECT cria_professor('Coelho', 'Teoria');
+SELECT cria_professor('Cris', 'Teoria');
+SELECT cria_professor('Roberto', 'Machie Learning');
+SELECT cria_professor('Nina', 'Machie Learning');
 
 SELECT cria_administrador('Jef', '20-05-2019', '20-05-2020');
-INSERT INTO administrador(id_adm, inicio, fim) VALUES
-	(12, '01-01-2010', '01-01-2040'),
-	(13, '01-01-2015', '01-01-2050'),
-	(14, '03-01-2016', '01-01-2050'),
-	(15, '20-05-2016', '20-01-2040'),
-	(16, '06-01-2017', '01-01-2050'),
-	(17, '07-01-2015', '01-01-2030'),
-	(18, '07-05-2018', '01-01-2030'),
-	(20, '01-05-2019', '01-01-2020'),
-	(21, '02-01-2019', '01-01-2022');
+SELECT cria_administrador('Carlinhos', '01-01-2010', '01-01-2040');
+SELECT cria_administrador('Hirata', '01-01-2015', '01-01-2050');
+SELECT cria_administrador('Renata', '03-01-2016', '01-01-2050');
+SELECT cria_administrador('Daniel', '20-05-2016', '20-01-2040');
+SELECT cria_administrador('Fabio', '06-01-2017', '01-01-2050');
+SELECT cria_administrador('Coelho', '07-01-2015', '01-01-2030');
+SELECT cria_administrador('Cris', '07-05-2018', '01-01-2030');
+SELECT cria_administrador('Nina', '01-05-2019', '01-01-2020');
+SELECT cria_administrador('Decio', '02-01-2019', '01-01-2022');
