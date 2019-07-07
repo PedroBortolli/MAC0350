fb681c2aae9c76af419c5a685e6b3a7e8e58674f
Pedro Sousa <pedroteosousa@gmail.com>
2019-07-07 02:13:41 -0300

    Add mod_cur to backend database

--- EP3/server/database.js
+++ EP3/server/database.js
@@ -1,16 +1,29 @@
 const Pool = require('pg').Pool
 
-const mod_acc = new Pool({
+// mudem esse objeto com as configurações de vcs
+// (n fiquem comitando essa porra se mudarem só isso, vai virar uma putaria, coloquem num .env da vida)
+const postgres_setup = {
     user: 'postgres',
     password: 'postgres',
     host: 'localhost',
     port: 5432,
+}
+
+const mod_acc = new Pool({
+    ...postgres_setup,
     database: 'mod_acc'
 })
 mod_acc.connect()
 
+const mod_cur = new Pool({
+    ...postgres_setup,
+    database: 'mod_cur',
+})
+mod_cur.connect()
+
 const client = {
     mod_acc,
+    mod_cur,
 }
 
 module.exports = client
